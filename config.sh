ae0b8f99d545003dbc75c966e882510ccfbeeaec
Chris Smith <chris87@gmail.com>
2016-05-15 18:22:48 +0100

    Fix staging check being backwards...

--- config.sh
+++ config.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 BASEDIR=/letsencrypt
-CONTACT_EMAIL="$EMAIL"
+CONTACT_EMAIL=$EMAIL
 
-if [[ -z "${STAGING:-}" ]]; then
-  CA="https://acme-staging.api.letsencrypt.org/directory"
-else
+if [ -z ${STAGING+-} ]; then
   CA="https://acme-v01.api.letsencrypt.org/directory"
+else
+  CA="https://acme-staging.api.letsencrypt.org/directory"
 fi
+
