482bb38681c2e33306e3da417a2fc57785464983
Chris Smith <chris87@gmail.com>
2016-05-21 12:24:12 +0100

    Correct iowait timeout (24 hours not 24 minutes)

--- run.sh
+++ run.sh
@@ -10,6 +10,6 @@ trap interrupt SIGINT
 
 while true; do
   /letsencrypt.sh --cron --hook /lexicon.sh --challenge dns-01 
-  inotifywait --timeout 1440 /letsencrypt/domains.txt
+  inotifywait --timeout 86400 /letsencrypt/domains.txt
   sleep 60
 done
